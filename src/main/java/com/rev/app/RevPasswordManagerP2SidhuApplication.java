a5d3699789d9e8d703a6b7765c8dd3cd97d94842
Sidhu <cherukusidhu@gmail.com>
2026-02-24 09:48:51 +0530

    New project Structure

---
+++ src/main/java/com/rev/app/RevPasswordManagerP2SidhuApplication.java
@@ -0,0 +1,13 @@
+package com.rev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevPasswordManagerP2SidhuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RevPasswordManagerP2SidhuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rev/app/RevPasswordManagerP2SidhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rev.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevPasswordManagerP2SidhuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
