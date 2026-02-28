8f4ca467a4d1038961631c9df82d063b3e35c060
Sidhu <cherukusidhu@gmail.com>
2026-02-28 17:27:01 +0530

    resources all modified

--- src/main/java/com/rev/app/config/SecurityConfig.java
+++ src/main/java/com/rev/app/config/SecurityConfig.java
@@ -77,7 +77,7 @@ public class SecurityConfig {
                                                                 "/login", "/register", "/auth/login", "/auth/register",
                                                                 "/recover/**", "/auth/verify-email", "/verify-email",
                                                                 "/auth/2fa-login", "/api/auth/**",
-                                                                "/css/**", "/js/**", "/images/**", "/favicon.ico")
+                                                        "/static.css/**", "/js/**", "/images/**", "/favicon.ico")
                                                 .permitAll()
                                                 .anyRequest().authenticated())
                                 .formLogin(form -> form
